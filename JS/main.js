e92b0eca82f28b7f724f0dd5f93af221338dc2e5
Iara Graells <iaragraells@gmai.com>
2023-10-11 18:53:00 -0300

    Agrego archivo data.json, asincronia y promesas, fetch

--- JS/main.js
+++ JS/main.js
@@ -3,119 +3,22 @@ const verCarrito = document.getElementById("verCarrito")
 const modalContainer = document.getElementById("modal-container")
 const cantidadCarrito = document.getElementById("cantidadCarrito")
 
-const categorias = ["novela", "ficcion", "romance", "juvenil", "policial", "aventura", "clasicos"]
-const productos = [
-    {
-        id: 1,
-        titulo: "El Gran Gatsby",
-        autor: "F. Scott Fitzgerald",
-        genero: "Novela, Ficción",
-        descripcion: "La historia del millonario Jay Gatsby y su obsesión por el pasado.",
-        rutaImagen: "https://m.media-amazon.com/images/I/61hR5wwWGuS._AC_UF1000,1000_QL80_.jpg",
-        precio: 2000,
-        unidades: 1
-    },
-    {
-        id: 2,
-        titulo: "Cien años de soledad",
-        autor: "Gabriel Garcia Marquez",
-        genero: "Novela",
-        descripcion: "La saga de la familia Buendía a lo largo de siete generaciones en el pueblo de Macondo.",
-        rutaImagen: "https://m.media-amazon.com/images/I/91TvVQS7loL._AC_UF1000,1000_QL80_.jpg",
-        precio: 2000,
-        unidades: 1
-    },
-    {
-        id: 3,
-        titulo: "Matar a un ruiseñor",
-        autor: "Harper Lee",
-        genero: "Novela, Ficción",
-        descripcion: "La historia de Atticus Finch y sus hijos mientras luchan contra el racismo en el sur de los Estados Unidos.",
-        rutaImagen: "https://m.media-amazon.com/images/I/51dxA6YXFzL._AC_UF1000,1000_QL80_.jpg",
-        precio: 2000,
-        unidades: 1
-    },
-    {
-        id: 4,
-        titulo: "1984",
-        autor: "George Orwell",
-        genero: "Novela, Ficción",
-        descripcion: "Una novela distópica que describe un mundo totalitario y vigilante.",
-        rutaImagen: "https://images.penguinrandomhouse.com/cover/9788418915093",
-        precio: 2000,
-        unidades: 1
-    },
-    {
-        id: 5,
-        titulo: "Orgullo y prejuicio",
-        autor: "Jane Austen",
-        genero: "Novela, Romance",
-        descripcion: "La historia de Elizabeth Bennet y su relación con el señor Darcy en la Inglaterra del siglo XIX.",
-        rutaImagen: "https://m.media-amazon.com/images/I/71wnBzT9WqL._AC_UF1000,1000_QL80_.jpg",
-        precio: 3000,
-        unidades: 1
-    },
-    {
-        id: 6,
-        titulo: "Don Quijote de la Mancha",
-        autor: "Miguel de Cervantes",
-        genero: "Clásico",
-        descripcion: "Las aventuras del caballero Don Quijote y su fiel escudero Sancho Panza en una sátira literaria.",
-        rutaImagen: "https://m.media-amazon.com/images/I/91zpNPYRFeL._AC_UF1000,1000_QL80_.jpg",
-        precio: 3000,
-        unidades: 1
-    },
-    {
-        id: 7,
-        titulo: "Los juegos del hambre",
-        autor: "Suzanne Collins",
-        genero: "Novela, Juvenil",
-        descripcion: "La historia de Katniss Everdeen en un mundo distópico donde jóvenes son forzados a luchar por su supervivencia en un juego mortal.",
-        rutaImagen: "https://m.media-amazon.com/images/I/51h3YpAz6rL._AC_UF1000,1000_QL80_.jpg",
-        precio: 1000,
-        unidades: 1
-    },
-    {
-        id: 8,
-        titulo: "Crónica de una muerte anunciada",
-        autor: "Gabriel Garcia Marquez",
-        genero: "Novela, Policial",
-        descripcion: "La narración de un asesinato en una pequeña comunidad y la inevitabilidad del mismo.",
-        rutaImagen: "https://images.cdn2.buscalibre.com/fit-in/360x360/13/97/1397a4f28df5ed21f99177884f3276bd.jpg",
-        precio: 1000,
-        unidades: 1
-    },
-    {
-        id: 9,
-        titulo: "Moby-Dick",
-        autor: "Herman Melville",
-        genero: "Novela, Ficción",
-        descripcion: "La obsesión del capitán Ahab por cazar al gran cachalote blanco conocido como Moby-Dick.",
-        rutaImagen: "https://m.media-amazon.com/images/I/81R91ODA9DL._AC_UF1000,1000_QL80_.jpg",
-        precio: 2000,
-        unidades: 1
-    }
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [] 
 
-]
-
-
-
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [] // LS //
-
-// Filtros //
+// Filtros - buscador //
 document.addEventListener("keyup", (e) => {
     if (e.target.matches(".buscador")) {
-        const filtro = e.target.value.toLowerCase();
+        const filtro = e.target.value.toLowerCase()
         document.querySelectorAll(".card").forEach((libro) => {
-            const textoLibro = libro.textContent.toLowerCase();
+            const textoLibro = libro.textContent.toLowerCase()
             if (
                 textoLibro.includes(filtro) ||
                 libro.querySelector(".autor").textContent.toLowerCase().includes(filtro) ||
                 libro.querySelector(".genero").textContent.toLowerCase().includes(filtro)
             ) {
-                libro.classList.remove("filtro");
+                libro.classList.remove("filtro")
             } else {
-                libro.classList.add("filtro");
+                libro.classList.add("filtro")
             }
         });
     }
@@ -123,11 +26,11 @@ document.addEventListener("keyup", (e) => {
 
 
 
-// filtros
+// filtros-botones
 
 document.addEventListener("DOMContentLoaded", function () {
-    const botonFiltros = document.querySelectorAll('.botonFiltro');
-    const productos = document.querySelectorAll('.genero');
+    const botonFiltros = document.querySelectorAll('.botonFiltro')
+    const productos = document.querySelectorAll('.genero')
 
     const categoriasAGeneros = {
         "todos": "todos",
@@ -141,13 +44,13 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     // Agrega la clase 'todos' al botón de filtro "Todos"
-    document.querySelector('.botonFiltro[value="todos"]').classList.add('todos');
+    document.querySelector('.botonFiltro[value="todos"]').classList.add('todos')
 });
 
 //A PARTIR DE ACA NO FUNCIONA -- REVISAR
 
 // Funcion para ocultar productos 
-/*    function hideProducts() {
+  /*  function hideProducts() {
         const cards = document.querySelectorAll(".card");
         cards.forEach(function(card) {
             card.style.transform = 'scale(0)';
@@ -155,53 +58,46 @@ document.addEventListener("DOMContentLoaded", function () {
                 card.style.display = 'none';
             }, 400);
         });
-    }
-
-    function showProducts(catProduct) {
-        productos.forEach(function(producto) {
-            const genero = producto.querySelector('.genero')
-
-            if (producto.genero === catProduct || catProduct === "todos") {
-                producto.style.display = 'block';
-                setTimeout(function() {
-                    producto.style.transform = 'scale(1)';
-                }, 400);
-            } else {
-                producto.style.display = 'none';
-            }
-        });
-    }
-    
-    // Agrega un evento 'click' a cada botón de filtro
-    botonFiltros.forEach(function(boton) {
-        boton.addEventListener('click', function() {
-            const catProduct = this.getAttribute('value');
-            console.log(catProduct);
+    } 
+   
+  
+    botonFiltros.forEach(function (boton) {
+        boton.addEventListener("click", function () {
+            const catProduct = this.getAttribute("value");
 
             // Remueve la clase 'todos' de todos los botones de filtro
-            botonFiltros.forEach(function(boton) {
-                boton.classList.remove('todos');
+            botonFiltros.forEach(function (boton) {
+                boton.classList.remove("todos");
             });
 
-            this.classList.add('todos');
+            this.classList.add("todos");
 
-            // Oculta los productos y luego muestra los que coinciden con la categoría
-            hideProducts();
-            setTimeout(function() {
-                showProducts(catProduct);
-            }, 400);
+            // Muestra los libros que coinciden con la categoría seleccionada
+            showProducts(catProduct);
         });
     });
 
+      function showProducts(catProduct) {
+        productos.forEach(function (producto) {
+            const genero = producto.genero.toLowerCase();
+
+            // Comprueba si el género del libro coincide con la categoría seleccionada o si es "todos"
+            if (catProduct === "todos" || genero.includes(catProduct)) {
+                producto.style.display = "block";
+                // Puedes aplicar aquí cualquier animación o estilo adicional si lo deseas
+            } else {
+                producto.style.display = "none";
+            }
+        });
+    }
 */
+    
 
+const getProducts = async ()=> {
+const response = await fetch("data.json")
+const data = await response.json()
 
-
-
-
-
-
-productos.forEach((product) => {
+data.forEach((product) => {
     let content = document.createElement("div") // creo cards de cada producto //
     content.className = "card" //nombre de clase para css//
     content.innerHTML = `
@@ -261,6 +157,10 @@ productos.forEach((product) => {
         }).showToast();
     })
 })
+}
+
+getProducts()
+
 
 // set item //
 const saveLocal = () => {
@@ -269,9 +169,9 @@ const saveLocal = () => {
 
 // get item //
 const loadLocal = () => {
-    const carritoString = localStorage.getItem("carrito");
+    const carritoString = localStorage.getItem("carrito")
     if (carritoString) {
-        carrito = JSON.parse(carritoString);
+        carrito = JSON.parse(carritoString)
     }
 }
 
@@ -358,6 +258,8 @@ const pintarCarrito = () => {
     modalContainer.append(totalCompra)
 
     // Botón "Finalizar Compra" //
+
+
     const finalizarCompraButton = document.createElement("button")
     finalizarCompraButton.textContent = "Finalizar Compra"
     finalizarCompraButton.className = "finalizar-compra-button"
@@ -378,12 +280,9 @@ const pintarCarrito = () => {
     finalizarCompraButton.addEventListener("click", function () {
         modalContainer.style.display = "none"
         modalPago.style.display = "block"
-    });
+    })
 
-};
-
-
-
+}
 
 verCarrito.addEventListener("click", pintarCarrito)
 
